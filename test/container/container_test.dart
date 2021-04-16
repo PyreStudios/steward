fa671eca82ff73647c74dd128a6d063e9a415be8
Brad Cypert <brad.cypert@gmail.com>
2021-04-16 18:00:04 -0400

    add another test to cover container objects building from other container bindings

--- test/container/container_test.dart
+++ test/container/container_test.dart
@@ -13,6 +13,19 @@ void main() {
       expect(key, 'ABC123');
     });
 
+    test('container items should be able to be built with other container items', () {
+      var container = Container();
+      container.bind('key', (Container container) {
+        return 'ABC';
+      });
+
+      container.bind('full_key', (Container container) {
+        return container.make('key') + '123';
+      });
+
+      expect(container.make('full_key'), 'ABC123');
+    });
+
     test('should throw an exception when an unbound binding is made', () {
       var container = Container();
       expect(() => container.make('key'), throwsNoSuchMethodError);
