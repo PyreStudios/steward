c3ac9fe0af276184e778a504ffdb6e6bd0e57b32
Brad Cypert <brad.cypert@gmail.com>
2022-01-12 19:30:43 -0500

    more cleanup

--- bin/steward.dart
+++ bin/steward.dart
@@ -1,7 +1,4 @@
-import 'package:steward/steward.dart' as steward;
-import './help.dart' as help;
 import './new.dart' as new_cmd;
-import './create.dart' as create_cmd;
 import 'package:bosun/bosun.dart' as bosun;
 
 void main(List<String> arguments) {
