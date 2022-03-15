84638be0e9c79b212f98f55d35079c364caba55e
Brad Cypert <brad.cypert@gmail.com>
2022-03-15 11:48:55 -0400

    start on new controllers, need bosun help to get much further

--- bin/new.dart
+++ bin/new.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:bosun/bosun.dart';
+import './new_controller.dart';
 
 var configTemplate = '''---
 app:
@@ -118,7 +119,8 @@ class NewCommand extends Command {
       : super(
             command: 'new',
             description: 'Create a new steward app',
-            example: 'steward new <app-name>');
+            example: 'steward new <app-name>',
+            subcommands: [NewControllerCommand()]);
 
   @override
   void run(List<String> args, Map<String, dynamic> flags) {
