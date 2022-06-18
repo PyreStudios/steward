42efd2344876bc7659707896f3738f8c8fcd1e0c
Brad Cypert <brad.cypert@gmail.com>
2022-06-17 20:40:31 -0400

    form docstrings

--- lib/forms/forms.dart
+++ lib/forms/forms.dart
@@ -1,9 +1,13 @@
+/// FormError represents an error found by a form.
+/// You'll maybe want to extend this to provide your own error types, but its usable as is, too.
 class FormError {
   String message;
 
   FormError(this.message);
 }
 
+/// Form is a base class for building your own forms.
+/// Please see the tests for examples on how they can be used.
 class Form {
   final List<FormError> Function(Form) _validator;
   bool _isValid = false;
